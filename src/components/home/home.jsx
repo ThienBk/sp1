56df28ca4645a8d10415a010179f2bf9cdef40d0
ThienBk <88071364+ThienBk@users.noreply.github.com>
2024-01-12 13:43:23 +0700

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -5,15 +5,10 @@ import homeBanner from "../../accessts/img/a-02.jpg"
 function Home(props) {
     return (
         <div style={{paddingTop: '50px'}} className='container mt-5'>
-            <h1 className='text-center mt-5'>Thần dược Trần Xuân Vương</h1>
+            <h1 className='text-center mt-5'>Mô tả bản thân</h1>
             <div className='mt-5'>
                 <h3 className='text-center'></h3>
-                <p>Trường Xuân Vương là một trong những sản phẩm tăng cường sinh lý nam giới được phát triển từ bác thuốc độc quyền của Bác sĩ, Lương y Nguyễn Hữu Trọng. Sản phẩm được bào chế từ các dược liệu quý, cải thiện sức khỏe tình dục nam giới chỉ sau 30 ngày. Vậy Trường Xuân 
-                Vương có tốt không? bao nhiêu một hộp, mua ở đâu tốt. Theo dõi ngay bài viết sau của chúng tôi!</p>
-            </div>
-            <h3>Xem chi tiết sản phẩm: <Link className='d-md-inline d-flex' style={{textDecoration : "none"}} to='/introduct'>Trần Xuân Vương</Link> </h3>
-            <div className="container"><img className='w-100' src={homeBanner} alt="" /></div>
-            <h3 className='text-center bg-dark text-white mt-5'><marquee scrollamount="10">Liên hệ với shop để được hỗ trợ mua hàng với giá ưu đãi</marquee></h3>
+                
             <div className='text-center my-5'>Make by: @Thiện21</div>
         </div>
     );
